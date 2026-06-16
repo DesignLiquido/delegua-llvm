0cb917e53994e1fa2ec05d201883966638dc2b9e
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-06-16 11:00:33 -0700

    Não mais testando classe estrangeira sem `@definicao`. Isso pode ser recolocado mais tarde.

--- testes/compilador-llvm.erros.test.ts
+++ testes/compilador-llvm.erros.test.ts
@@ -40,13 +40,4 @@ describe('Compilador - Erros', () => {
         ])).rejects.toThrow("Erro sintático: Variável não definida: 'naoDeclarada'.");
     });
 
-    it('Classe estrangeira sem @definicao deve lançar erro de compilação', async () => {
-        const compilador = new CompiladorLLVM();
-
-        await expect(compilador.compilar([
-            'classe estrangeira LibC {',
-            '    puts(s: texto): inteiro',
-            '}'
-        ])).rejects.toThrow("Classe estrangeira 'LibC' não tem @definicao(biblioteca=\"...\").");
-    });
 });
